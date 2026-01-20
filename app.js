6cbbf424e94142644d6b3c0408951222a7b856d2
Daum <nikos.daum@bahag.com>
2026-01-20 13:32:05 +0100

    Phase 2.2 Del Button und Updates

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 
+const APP_VERSION = 1;
+
 let dates = [];
 
 function loadDates() {
+
+    const storedVersion = localStorage.getItem("appVersion");
+
+    if (storedVersion != APP_VERSION) {
+        localStorage.removeItem("dates");
+        localStorage.setItem("appVersion", APP_VERSION);
+    }
+
     const storedDates = localStorage.getItem('dates');
     dates = storedDates ? JSON.parse(storedDates) : [];
 }
@@ -16,11 +26,21 @@ function renderDates() {
     dates.forEach(date => {
         const li = document.createElement('li');
         li.textContent = `${date.title}`;
+
+        const del = document.createElement('button');
+        del.textContent = 'X';
+        del.addEventListener('click', () => {
+            dates = dates.filter(d => d.id !== date.id);
+            saveDates();
+            renderDates();
+        });
+
+        li.appendChild(del);
         dateList.appendChild(li);
     });
 }
 
-document.getElementById('addDateForm').addEventListener('submit', function(event) {
+document.getElementById('addDateForm').addEventListener('submit', function (event) {
     event.preventDefault();
     const input = document.getElementById('TitleInput');
     const newDate = {
